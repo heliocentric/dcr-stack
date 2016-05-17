e3f7dfd8800576ddb85a59caef458273350bf320
Dylan Cochran <heliocentric@gmail.com>
2016-05-17 13:56:03 -0400

    Add management port

--- puppet/main.pp
+++ puppet/main.pp
@@ -14,6 +14,7 @@ class { "::erlang":
 } ->
 class  {"::rabbitmq":
 	package_provider => "rpm",
+	management_port => "15672",
 	package_source => 'https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.1/rabbitmq-server-3.6.1-1.noarch.rpm',
 } ->
 class  {"::consul":
